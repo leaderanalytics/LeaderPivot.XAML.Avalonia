972c3001d3ae9cd1fa4d7b4bf2c51392f4ea70cc
Sam <sam.wheat@outlook.com>
2022-05-07 12:15:10 -0700

    Initial commit

---
+++ LeaderPivot.XAML.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,9 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeaderPivot.XAML.Avalonia.ViewModels;
+public class ViewModelBase : ReactiveObject
+{
+}
